7c7d76e4ff357ca4662dcee2bc00b996358bab0c
Deolu_Josef <kiishijoseph@gmail.com>
2021-08-12 12:19:37 +0100

    added more views to the frontend django app

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,7 @@ from frontend import views
 app_name = 'frontend'
 
 urlpatterns = [
+    path('home/', views.home, name='home'),
     path('playlist/', views.playlist, name='playlist'),
     path('team/', views.team , name= 'team'),
     path('contact/', views.contact, name='contact'),
--- frontend/views.py
+++ frontend/views.py
@@ -8,17 +8,17 @@ def home(request):
     return HttpResponse('<h1 style = "text-align: center; margin-top: 25%; font-weight : 900"> Welcome😃 </h>')
 
 def playlist(request):
-    return HttpResponse ('playlist')
+    return HttpResponse ('<h1 style = "text-align: center; margin-top: 25%; font-weight : 900"> Playlist🎵🎧 </h>')
 
 def team(request):
-    return HttpResponse('Our team')
+    return HttpResponse('<h1 style = "text-align: center; margin-top: 25%; font-weight : 900"> Our Team👨‍👩‍👦‍👦 </h>')
 
 def contact(request):
-    return HttpResponse ('contact us')
+    return HttpResponse ('<h1 style = "text-align: center; margin-top: 25%; font-weight : 900"> Contact Us💌 </h>')
 
 def login (request):
-    return HttpResponse('login')
+    return HttpResponse('<h1 style = "text-align: center; margin-top: 25%; font-weight : 900"> Protect your account 👁‍🗨 </h>')
 
 def register(request):
-    return HttpResponse ('register')
+    return HttpResponse ('<h1 style = "text-align: center; margin-top: 25%; font-weight : 900"> Join us 🚀 </h>')
 
